1248d04ce660dddb3b7dd0619c7fdc10bc8bd502
Nikit <nikit.rai@internationalsos.com>
2025-08-03 17:56:37 +0530

    chore: Added Health Controller and refracted yaml files

---
+++ Controller/HealthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductService.Controller
+{
+    [ApiController]
+    [Route("health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok(new { status = "Healthy", service = "ProductService", timestamp = DateTime.UtcNow });
+
+    }
+}
